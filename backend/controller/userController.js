9d3085c548454e51274b89278857a15ca0c7029a
neway-k <newksm21@gmail.com>
2025-09-29 22:22:27 +0300

    my first commit

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,18 @@
-const register= (req,res)=>{
-res.json({register:'register'})
+const db=require('../db/dbConfig')
+
+
+
+const register= async (req,res)=>{
+  const {	username,	firstname,	lastname,	email,	password	
+}=req.body
+if (!username || !firstname || !lastname || !email || !password) {
+  res.status(400).json({ error: "All fields are required" });
+}
+try {
+  
+} catch (error) {
+  res.status(500).json({ error: "Server error" });
+}
 }
 
 const login = (req, res) => {
