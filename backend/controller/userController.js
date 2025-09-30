a86a153dfbc97add6896705b27cb69b5fbef1da0
kibran meng <kibran29mengistu@gmail.com>
2025-09-30 23:56:43 +0300

    Implement login and register routes with JWT

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,6 +1,7 @@
 const db = require("../db/dbConfig");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { StatusCodes } = require("http-status-codes");
 
 // register
 const register = async (req, res) => {
@@ -8,8 +9,10 @@ const register = async (req, res) => {
   console.log(" Register request received:", req.body);
 
   if (!username || !firstname || !lastname || !email || !password) {
-    console.log(" Missing registration fields");
-    return res.status(400).json({ error: "All fields are required" });
+    // console.log(" Missing registration fields");
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: "All fields are required" });
   }
 
   try {
@@ -17,27 +20,39 @@ const register = async (req, res) => {
       "SELECT * FROM users WHERE email = ?",
       [email]
     );
-    console.log(" Existing user check:", existingUser);
+    // console.log(" Existing user check:", existingUser);
 
     if (existingUser.length > 0) {
-      console.log(" Email already registered:", email);
-      return res.status(409).json({ error: "Email already registered" });
+      // console.log(" Email already registered:", email);
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: "Email already registered" });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(" Hashed password:", hashedPassword);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    // console.log(" Hashed password:", hashedPassword);
 
     await db.query(
       "INSERT INTO users (username, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?)",
       [username, firstname, lastname, email, hashedPassword]
     );
+    const [user] = await db.query("select userid from users where email=?", [
+      email,
+    ]);
+    const userid = user[0].userid;
+    const token = jwt.sign({ userid, username }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
 
-    console.log("User registered");
-    return res.status(201).json({ message: "User registered successfully" });
+    // console.log("User registered");
+    return res.status(StatusCodes.CREATED).json({
+      message: "User registered successfuly!",
+      token,
+    });
   } catch (error) {
     console.error(" Register error:", error.message);
     return res
-      .status(500)
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json({ error: "Server error", details: error.message });
   }
 };
@@ -45,53 +60,48 @@ const register = async (req, res) => {
 // login
 const login = async (req, res) => {
   const { email, password } = req.body;
-  console.log(" Login request received:", req.body);
+  // console.log(" Login request received:", req.body);
 
   if (!email || !password) {
     console.log(" Missing login fields");
-    return res.status(400).json({ error: "Email and password required" });
+    return res.status(StatusCodes.BAD_REQUEST).json({ error: "Email and password required" });
   }
 
   try {
-    const [rows] = await db.query("SELECT * FROM users WHERE email = ?", [
-      email,
-    ]);
-    console.log(" DB query result:", rows);
+    const [rows] = await db.query(
+      "SELECT userid,username,password FROM users WHERE email = ?",
+      [email]
+    );
 
     const user = rows[0];
+    // console.log(user);
     if (!user) {
-      console.log(" User not found:", email);
-      return res.status(404).json({ error: "User not found" });
+      // console.log(" User not found:", email);
+      return res.status(StatusCodes.UNAUTHORIZED).json({ error: "User not found" });
     }
 
+    const { userid, username } = user;
     const isMatch = await bcrypt.compare(password, user.password);
-    console.log(" Password match:", isMatch);
+    // console.log(" Password match:", isMatch);
 
     if (!isMatch) {
-      console.log(" Invalid password");
-      return res.status(401).json({ error: "Invalid credentials" });
+      // console.log(" Invalid password");
+      return res.status(StatusCodes.UNAUTHORIZED).json({ error: "Invalid credentials" });
     }
 
-    const token = jwt.sign(
-      { id: user.userid, email: user.email },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-    console.log(" JWT generated");
+    const token = jwt.sign({ userid, username }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    // console.log(" JWT generated");
 
-    return res.status(200).json({
+    return res.status(StatusCodes.OK).json({
       message: "Login successful!",
       token,
-      user: {
-        id: user.userid,
-        username: user.username,
-        email: user.email,
-      },
     });
   } catch (error) {
     console.error("Login error:", error.message);
     return res
-      .status(500)
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json({ error: "Server error", details: error.message });
   }
 };
