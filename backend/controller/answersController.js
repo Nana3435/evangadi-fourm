dd5fec937ac82e1bcb7318a5d744e2045908d2ac
Faruk Ahmed <far12345ahmed@gmail.com>
2025-09-30 11:23:20 +0300

    adding api for posting an answer for a question

--- backend/controller/answersController.js
+++ backend/controller/answersController.js
@@ -1,8 +1,26 @@
+const db =require('../db/dbConfig')
 
 
+const postAnswer =async (req,res)=>{
+  const {questionid} = req.params
+  const {userid,answer} = req.body
+  
 
-const postAnswer = (req,res)=>{
-res.json({postAnswer:'post answer'})
+  if (!answer) {
+    return res.status(400).json({msg:"please fill the answer field"})
+  }
+  try {
+    const [questions] = await db.query(
+      "select questionid from questions where questionid=?",[questionid]
+    );
+    if (questions.length===0) {
+      return res.status(400).json({msg:'this question is no more available'})
+    }
+     await db.query("insert into answers(userid,questionid,answer) values (?,?,?)",[userid,questionid,answer]);
+     return res.status(202).json({msg:"answer posted successfully"})
+  } catch (error) {
+    res.status(500).json({msg:"something goes wrong please try again later!"})
+  }
 }
 
 const getAnswer = (req, res) => {
--- backend/routes/answerRoute.js
+++ backend/routes/answerRoute.js
@@ -3,7 +3,7 @@ const { postAnswer, getAnswer } = require("../controller/answersController");
 const answerRouter = express.Router()
 
 
-answerRouter.post("/post-answer", postAnswer);
+answerRouter.post("/post-answer/:questionid", postAnswer);
 answerRouter.get("/get-answer", getAnswer);
 
 module.exports= answerRouter
